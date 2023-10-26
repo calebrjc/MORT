6770b1c197b654564e99253f1fefb501dac06650
Caleb Johnson-Cantrell <46861012+calebrjc@users.noreply.github.com>
2023-10-26 16:55:17 -0400

    feat(usart): Restructure drivers into a BSP; implement usart driver

--- src/bsp_atmega328p/src/io/io.c
+++ src/bsp_atmega328p/src/io/io.c
@@ -1,4 +1,4 @@
-#include "io.h"
+#include "bsp/io/io.h"
 
 #include <avr/io.h>
 
--- src/main.c
+++ src/main.c
@@ -1,19 +1,50 @@
+#include <avr/interrupt.h>
+#include <stddef.h>
 #include <util/delay.h>
 
 #include "bsp/io/io.h"
+#include "bsp/usart/usart.h"
+
+void toggle_pb0(char c) {
+    (void)c;
+
+    io_toggle(BSP_PB0);
+}
+
+void write_string(const char *s, size_t size) {
+    for (size_t i = 0; i < size; i++) { usart_write(BSP_USART0, s[i]); }
+}
 
 int main(void) {
-    io_configure(IO_PB0, (io_config){.direction = IO_OUTPUT, .initial_level = IO_HIGH});
-    io_configure(IO_PB5, (io_config){.direction = IO_OUTPUT, .initial_level = IO_LOW});
-    io_configure(IO_PB1, (io_config){.direction = IO_INPUT, .resistor = IO_PULLUP});
+    sei();
+
+    io_configure(BSP_PB0, (io_config){.direction = IO_OUTPUT, .initial_level = IO_HIGH});
+
+    usart_init(BSP_USART0, (usart_config){.baud_rate = USART_BAUD_9600, .echo_on_recv = true});
+    usart_write(BSP_USART0, '>');
+    usart_register_callback(BSP_USART0, toggle_pb0);
 
     while (1) {
         // PB5 = PB1
-        io_write(IO_PB5, io_read(IO_PB1));
+        // io_write(IO_PB5, io_read(IO_PB1));
 
-        io_toggle(IO_PB0);
+        //    io_toggle(PB0);
 
-        _delay_ms(500);
+        // if (usart_poll()) {
+        //     char c = usart_read();
+
+        //     if (c == '1') {
+        //         io_write(IO_PB0, IO_HIGH);
+        //     } else if (c == '0') {
+        //         io_write(IO_PB0, IO_LOW);
+        //     }
+
+        //     usart_write(c);
+
+        //     if (c == '\r') { usart_write('\n'); }
+        // }
+
+        // _delay_ms(500);
     }
 
     return 0;
