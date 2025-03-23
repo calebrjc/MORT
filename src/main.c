961d54da244390ce3623b1e506aec72d9dee8d2e
Caleb Johnson-Cantrell <46861012+calebrjc@users.noreply.github.com>
2025-03-22 21:16:21 -0400

    feat: Implement a driver for the TB6612 for dual DC motors

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
 #include "driver/nec.h"
+#include "driver/tb6612fng_dc.h"
 #include "hal/counter.h"
 #include "hal/gpio.h"
 #include "hal/pwm.h"
@@ -45,23 +45,90 @@ int main(void)
     ec = mort_pwm_init(MORT_PWM_CH_MOTORL);
     MORT_ASSERT_MSG(ec == 0, "Failed to initialize the PWM");
 
-    ec = mort_pwm_set_duty_cycle(MORT_PWM_CH_MOTORR, 50);
-    MORT_ASSERT_MSG(ec == 0, "Failed to set the duty cycle");
+    mort_tb6612fng_dc_motor_init_t motora_init = {
+        MORT_PWM_CH_MOTORR, MORT_GPIO_PIN_MOTOR_AIN1, MORT_GPIO_PIN_MOTOR_AIN2};
+    mort_tb6612fng_dc_motor_init_t motorb_init = {
+        MORT_PWM_CH_MOTORL, MORT_GPIO_PIN_MOTOR_BIN1, MORT_GPIO_PIN_MOTOR_BIN2};
+    ec = mort_tb6612fng_dc_init(&motora_init, &motorb_init, MORT_GPIO_PIN_MOTOR_STBY);
+    MORT_ASSERT_MSG(ec == 0, "Failed to initialize the TB6612FNG_DC");
 
-    ec = mort_pwm_set_duty_cycle(MORT_PWM_CH_MOTORL, 50);
-    MORT_ASSERT_MSG(ec == 0, "Failed to set the duty cycle");
+    mort_tb6612fng_dc_set_standby(false);
+    mort_tb6612fng_dc_set_mode(MORT_TB6612FNG_DC_MOTOR_A, MORT_TB6612FNG_DC_MODE_CW);
+    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_A, 100);
 
     while (1)
     {
-        mort_gpio_toggle(MORT_GPIO_PIN_MOTOR_AIN1);
-        mort_gpio_toggle(MORT_GPIO_PIN_MOTOR_AIN2);
-        mort_gpio_toggle(MORT_GPIO_PIN_MOTOR_BIN1);
-        mort_gpio_toggle(MORT_GPIO_PIN_MOTOR_BIN2);
-        mort_gpio_toggle(MORT_GPIO_PIN_MOTOR_STBY);
+        if (!s_printed)
+        {
+            MORT_LOGN("Button %s", mort_nec_button_to_string(s_last_button_pressed));
+            s_printed = true;
 
-        k_sleep(K_MSEC(500));
+            switch (s_last_button_pressed)
+            {
+                case MORT_NEC_BTN_UP:
+                {
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_A, MORT_TB6612FNG_DC_MODE_CW);
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_B, MORT_TB6612FNG_DC_MODE_CCW);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_A, 40);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_B, 40);
+                    break;
+                }
+
+                case MORT_NEC_BTN_DOWN:
+                {
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_A, MORT_TB6612FNG_DC_MODE_CCW);
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_B, MORT_TB6612FNG_DC_MODE_CW);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_A, 40);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_B, 40);
+                    break;
+                }
+
+                case MORT_NEC_BTN_LEFT:
+                {
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_A, MORT_TB6612FNG_DC_MODE_SHORT_BRAKE);
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_B, MORT_TB6612FNG_DC_MODE_CCW);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_A, 40);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_B, 40);
+                    break;
+                }
+
+                case MORT_NEC_BTN_RIGHT:
+                {
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_A, MORT_TB6612FNG_DC_MODE_CW);
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_B, MORT_TB6612FNG_DC_MODE_SHORT_BRAKE);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_A, 40);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_B, 40);
+                    break;
+                }
+
+                case MORT_NEC_BTN_NONE:
+                {
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_A, MORT_TB6612FNG_DC_MODE_STOP);
+                    mort_tb6612fng_dc_set_mode(
+                        MORT_TB6612FNG_DC_MOTOR_B, MORT_TB6612FNG_DC_MODE_STOP);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_A, 0);
+                    mort_tb6612fng_dc_set_speed(MORT_TB6612FNG_DC_MOTOR_B, 0);
+                    break;
+                }
+
+                default:
+                    break;
+            }
+        }
+
+        k_sleep(K_MSEC(16));
     }
 #endif
+
     // NOTE(Caleb): We should never get to this point
     return 0;
 }
--- src/util/compiler.h
+++ src/util/compiler.h
@@ -1,3 +1,11 @@
 #pragma once
 
-#define MORT_UNUSED(x) ((void)(x))
+#define MORT_UNUSED(__x) ((void)(__x))
+
+#ifdef __GNUC__
+#define MORT_LIKELY(__x)   __builtin_expect(!!(__x), 1)
+#define MORT_UNLIKELY(__x) __builtin_expect(!!(__x), 0)
+#else
+#define MORT_LIKELY(__x)   (__x)
+#define MORT_UNLIKELY(__x) (__x)
+#endif
